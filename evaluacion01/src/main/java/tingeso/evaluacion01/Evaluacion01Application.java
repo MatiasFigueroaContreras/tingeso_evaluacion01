6b2b2619e6de9e4d7120dab89fe1b32b0a3617b8
Matías Figueroa Contreras <matias.figueroa.c@usach.cl>
2023-03-26 16:02:21 -0300

    Archivos Iniciales

---
+++ evaluacion01/src/main/java/tingeso/evaluacion01/Evaluacion01Application.java
@@ -0,0 +1,13 @@
+package tingeso.evaluacion01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Evaluacion01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Evaluacion01Application.class, args);
+	}
+
+}
---
+++ evaluacion01/src/test/java/tingeso/evaluacion01/Evaluacion01ApplicationTests.java
@@ -0,0 +1,13 @@
+package tingeso.evaluacion01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Evaluacion01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
